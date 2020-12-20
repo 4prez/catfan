b3760bc92b257e450c80d7d7198fecb3a8eef1d7
Ed <ed4prez@yahoo.com>
2020-12-20 08:46:12 -0600

    Default Changelist

--- catfin02.py
+++ catfin02.py
@@ -1,12 +1,13 @@
-# description
+# DESCRIPTION
 # RPi program to run a fan based on distance sensor trip or manual intervention
 # Button: if not running - run as trigger, if running - stop running
 # Sensor: based on depth, if not running trigger fan
 # Blackout: time window where Sensor does not trip
 
-# structure
+# STRUCTURE
+# load general packages
 # check if running on Pi
-# load packages
+# load Pi packages / GPIO setup
 # log boot
 # initiate variables
 # start loop
@@ -19,8 +20,7 @@
 
 
 
-# load packages
-#
+# LOAD PACKAGES
 import datetime as dt
 import os
 import sys
@@ -54,15 +54,52 @@ with open(startup_csv_path, 'a') as fd:
 
 # initalize variables
 Exit_Now = False
-write_freq = 5  # in seconds
-ctrl_freq = write_freq
-time_last_write = dt.datetime.now()
+fan_duration = 10 # in seconds
+fan_run = False
+fan_trigger_distance = True
+fan_trigger_button = False
+black_out_start_01 = dt.time(3, 25, 56)
+black_out_duration_01 = 15 # minutes
+# black_out_end_01 = black_out_start_01 + black_out_duration_01*60
+black_out_start_02 = dt.time(15, 25, 0)
+black_out_duration_02 = 15 # minutes
+# black_out_end_02 = black_out_start_02 + timedelta(minutes=black_out_duration_02)
+# fan_end_time =  dt.datetime.now()
+
+fan_end_time = dt.datetime.now() + dt.timedelta(seconds=fan_duration)
 
 # loop start
 while not Exit_Now:
     print(dt.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+
+    # check for triggers
+    # button
+    # fan_end_time = dt.datetime.now() + dt.timedelta(seconds=fan_duration)
+    # fan_trigger_distance = True
+    # distance
+    # ignore if during blackout
+
+
+
+    # fan control
+    # fan control - check timer
+
+    if fan_run == True and dt.datetime.now() > fan_end_time:
+        fan_run = False
+
+
+    if fan_trigger_distance == True:
+        fan_run = True
+        fan_trigger_distance = False
+
+    if fan_trigger_button == True and fan_run == True:
+        fan_run = not(False)
+        fan_trigger_button = False
+
+    print(fan_run)
+
+    # prepare for loop
     time.sleep(1)
 
 if On_Raspberry == True:
     GPIO.cleanup()
-    lcd.lcd_clear()
