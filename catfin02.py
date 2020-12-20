8a6196f71724b82f5fd9aa8d1c400de682ed98eb
Ed <ed4prez@yahoo.com>
2020-12-20 11:36:43 -0600

    Default Changelist

--- catfin02.py
+++ catfin02.py
@@ -35,6 +35,16 @@ if On_Raspberry == True:
     print("On Rasp")
     import RPi.GPIO as GPIO
 
+    GPIO.setmode(GPIO.BOARD)
+
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
+
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
+
+
+
 path_abs = os.path.dirname(os.path.abspath(__file__))
 print(path_abs)
 
@@ -77,6 +87,30 @@ while not Exit_Now:
     # fan_end_time = dt.datetime.now() + dt.timedelta(seconds=fan_duration)
     # fan_trigger_distance = True
     # distance
+    if On_Raspberry == True:
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Waiting for sensor to settle")
+
+        time.sleep(2)
+
+        print("Calculating distance")
+
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+        time.sleep(0.00001)
+
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        while GPIO.input(PIN_ECHO) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO) == 1:
+            pulse_end_time = time.time()
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance:", distance, "cm")
+
+
     # ignore if during blackout
 
 
@@ -103,3 +137,6 @@ while not Exit_Now:
 
 if On_Raspberry == True:
     GPIO.cleanup()
+
+
+
