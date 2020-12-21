637426007a822f163cb8ec4d7287c4c143cec84c
Ed <ed4prez@yahoo.com>
2020-12-20 19:56:29 -0600

    Default Changelist

--- LED.py
+++ LED.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-FAN_PIN = 13
+FAN_PIN = 25
 GPIO.setup(FAN_PIN,GPIO.OUT)
 print("LED on")
 GPIO.output(FAN_PIN,GPIO.HIGH)
