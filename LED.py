1dea703e56c9e327e4d526d33e46230402b1b6ab
Ed <ed4prez@yahoo.com>
2020-12-20 19:49:43 -0600

    Default Changelist

--- LED.py
+++ LED.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
 print("LED on")
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(26,GPIO.HIGH)
 time.sleep(1)
 print("LED off")
-GPIO.output(18,GPIO.LOW)
+GPIO.output(26,GPIO.LOW)
+GPIO.cleanup()
